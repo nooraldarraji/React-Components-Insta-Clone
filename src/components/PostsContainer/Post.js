77ddf963b564f92bdcc4ec1f672c68a37298b066
Noor Al Darraji <077068@yahoo.com>
2020-03-23 23:30:38 -0500

    This suppose to be an MVP :fire:

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,7 +9,16 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+  const [likes2, setLikes2] = useState(props.likes)
+  const { post } = props
+  // const ll = props.likes
+  // console.log(likes2, 'likessss')
 
+  // console.log(ll, 'likes before fun')
+  // const iLikes = () => {
+  //   console.log(ll, 'likes inside function')
+  //   return (ll > props.likes) ? setLikes(props.likes + 1) : setLikes(props.likes - 1)
+  // }
   return (
     <div className="post-border">
       <PostHeader
@@ -25,8 +34,8 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
-      <CommentSection
+      <LikeSection likes={likes2} setLikes={setLikes2} />
+      <CommentSection comments={props.post.comments}
         postId={props.post.imageUrl}
         comments={props.post.comments}
       />
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import Data from '../../dummy-data'
 
 const PostsPage = () => {
   // set up state for your data
+  const [data, setData] = useState(Data)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(i =>
+
+        <Post post={i} />
+      )}
     </div>
   );
 };
